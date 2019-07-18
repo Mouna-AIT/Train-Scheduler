eb83e788ee4b4bc342deb81bc91ae239b6a31479
Moumix <mouna.aitichou@gmail.com>
2019-07-18 00:35:04 -0400

    added basic html and files

---
+++ assets/script/app.js
@@ -0,0 +1,31 @@
+// Firebase config
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAwMGY0yvHL4pFaW6ZsLKq7S7wqPp8JpZ4",
+    authDomain: "train-scheduler-85a26.firebaseapp.com",
+    databaseURL: "https://train-scheduler-85a26.firebaseio.com",
+    projectId: "train-scheduler-85a26",
+    storageBucket: "",
+    messagingSenderId: "1054613875682",
+    appId: "1:1054613875682:web:9f60a747d3ef76e9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+//  var 
+
+var database = firebase.database();
+
+
+
+
+
+
+
+
+
+
+
+
+});
